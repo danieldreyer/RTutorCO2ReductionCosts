65a34da2f83a273422b99dfbdaa51f9edda72920
Daniel Dreyer <dnl.dreyer@googlemail.com>
2020-03-16 01:39:07 +0100

    backup

---
+++ dp.R
@@ -0,0 +1,15 @@
+show.regression= function(...){
+  library(stargazer)
+    stargazer(..., 
+            type = "text", 
+            style = "aer",  
+            digits = 3,
+            df = FALSE,
+            report = "vct*",
+            star.cutoffs = c(0.05, 0.01, 0.001),
+            model.names = FALSE,
+            object.names = TRUE,
+            model.numbers = FALSE, 
+            omit.stat=c("f", "ser")
+    )
+} 
